3c9cef90559be045277823d8a5c9cc2d6ae27c5f
Ashish Rawat <ashishr350@gmail.com>
2019-08-11 19:13:55 +0530

    Ashish | fix basic layouting

--- src/components/appThemeSelection.js
+++ src/components/appThemeSelection.js
@@ -5,14 +5,19 @@ const ThemeToggleSwitch = () => (
     <ThemeConsumer>
         {
             ({toggleTheme}) => (
-                <div className="switch">
-                    <label>
-                        dark
-                        <input type="checkbox" onClick={toggleTheme} />
-                        <span className="lever"></span>
-                        light
-                    </label>
+                <div className="row">
+                    <div className="col m2 offset-m10">
+                        <div className="switch">
+                            <label>
+                                dark
+                                <input type="checkbox" onClick={toggleTheme} />
+                                <span className="lever"></span>
+                                light
+                            </label>
+                        </div>
+                    </div>
                 </div>
+
             )
         }
     </ThemeConsumer>
